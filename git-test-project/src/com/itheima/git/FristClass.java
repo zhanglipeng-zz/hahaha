735bec8d38fc75af3bc64a121b64630be00640c7
jaz <1650768691@qq.com>
2019-10-16 23:03:18 +0800

    把本地工程添加到GIT

--- git-test-project/src/com/itheima/git/FristClass.java
+++ git-test-project/src/com/itheima/git/FristClass.java
@@ -4,4 +4,9 @@ public class FristClass {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+    public void first(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
 }
